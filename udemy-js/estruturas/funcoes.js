eefa0b3d43638a673ee6c7b9a1118363348b63df
Pietra Ataulo <pietraataulo@gmail.com>
2024-11-29 12:08:17 -0300

    estruturas

---
+++ udemy-js/estruturas/funcoes.js
@@ -0,0 +1,21 @@
+// funções built-in
+
+// prompt
+
+let idade = prompt("Qual a sua idade?")
+console.log(idade)
+
+// alert
+
+alert("O pica-pau está na sacada")
+
+// Math.x
+
+let maior = Math.max(6,12,28,55,8)
+console.log(maior)
+
+let menor = Math.min(6,12,28,55,8)
+console.log(menor)
+
+let arredondar = Math.round(5.2998447)
+console.log(arredondar)
